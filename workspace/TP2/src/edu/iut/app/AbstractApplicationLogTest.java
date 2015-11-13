6c17b95821e82f126d686c7c990e2e55a9ef6c95
Zhang-Etienne <goldenfarfalla@gmail.com>
2015-11-13 10:55:37 +0100

    Ajout du répertoire workspace

---
+++ workspace/TP2/src/edu/iut/app/AbstractApplicationLogTest.java
@@ -0,0 +1,99 @@
+package edu.iut.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AbstractApplicationLogTest {
+
+	@Test
+	public void testAbstractApplicationLog() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddListener() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetApplicationLogListeners() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFireMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
